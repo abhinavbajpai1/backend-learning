a83ba29f7d5109fece89ecaf131011d3a1af7796
abhinav bajpai <abhinavbajpai045@gmail.com>
2025-06-18 20:00:43 +0530

    multer and middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,22 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({ 
+  cloud_name:  process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_key,
+  api_secret:process.env.CLOUDINARY_SECRET
+});
+
+const uploadONCloudinary = async (localFilepath)=>{
+    try {
+        if(!localFilepath) return null
+        const response= await cloudinary.uploader.upload(localFilepath,{
+            resource_type:"auto"
+        })
+        console.log("file uploaded successfully",response.url);
+        return response;
+    } catch (error) {
+        fs.unlink(localFilepath)
+    }
+}
+export {uploadONCloudinary};
